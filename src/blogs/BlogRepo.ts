bd187b8b77f0d609d5e5826079b23d0c6ffcd842
chronicler-jacob-stafford <chronicler.jacob.staff@outlook.com>
2025-05-23 08:47:33 -0500

    Add Dist to host site on GitHub
    GitHub hostable!

--- src/blogs/BlogRepo.ts
+++ src/blogs/BlogRepo.ts
@@ -1,5 +1,4 @@
 import Blog from '@/types/Blog';
-import tailwindImg from '@/assets/tailwind.jfif';
 import creatingThisBlogSite from "@/blogs/CreatingThisBlogSite/CreatingThisBlogSite";
 
 const BlogRepo: Blog[] = [
